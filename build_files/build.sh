022dc529b28d38a46bb114541734dd6a2d94667e
Pikalyx <pikalegend789@gmail.com>
2026-08-29 23:12:43 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,42 +5,40 @@ set -ouex pipefail
 cp -avf "/ctx/system_files"/. /
 
 dnf5 -y remove plasma-workspace plasma-* kde-*
-dnf5 -y copr enable zhangyi6324/noctalia-shell
 
-dnf5 -y install 				    \
-	niri						          \
-	gnome-keyring				      \
-	dolphin						        \
-	xwayland-satellite		    \
-	ark							          \
-	mako						          \
-	mpv							          \
-	unrar						          \
-	gdm							          \
+dnf5 -y install 				\
+	niri						\
+	ghostty						\
+	gnome-keyring				\
+	dolphin						\
+	xwayland-satellite			\
+	noctalia-shell				\
+	librewolf					\
+	ark							\
+	mako						\
+	mpv							\
+	unrar						\
+	gdm							\
 	xdg-desktop-portal-gtk		\
 	xdg-desktop-portal-gnome	\
-	cifs-utils					      \
-	kio-fuse					        \
-	kio-extras					      \
-	dolphin-plugins				    \
-	audiocd-kio					      \
-	kf5-kimageformats			    \
+	cifs-utils					\
+	kio-fuse					\
+	kio-extras					\
+	dolphin-plugins				\
+	audiocd-kio					\
+	kf5-kimageformats			\
 	kdegraphics-thumbnailers	\
-	ffmpegthumbs				      \
-	icoutils					        \
-	taglib						        \
-	cliphist					        \
-	ddcutil						        \
-	polkit-kde
+	ffmpegthumbs				\
+	icoutils					\
+	taglib						\
+	cliphist					\
+	ddcutil						\
+	polkit-kde					\
+	gnome-keyring
 
 
-if ! dnf5 -y install ghostty; then
-	echo "Warning: ghostty unavailable, continuing without it."
-fi
 
-if ! dnf5 -y install noctalia-shell; then
-	echo "Warning: noctalia-shell unavailable due to repo/dependency state, continuing without it."
-fi
+
 
 
 systemctl enable podman.socket
