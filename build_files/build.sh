01f473c2344c8737db4fbf24da2c8fbf8dba4244
Pikalyx <pikalegend789@gmail.com>
2026-08-30 20:28:05 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,46 +2,23 @@
 
 set -ouex pipefail
 
-cp -avf "/ctx/system_files"/. /
+### Install packages
 
-dnf5 -y remove plasma-workspace plasma-* kde-*
-curl -fsSL https://install.danklinux.com | sh -s -- -c niri -t ghostty -y
-dnf5 config-manager setopt terra.enabled=1
-dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 -y install 				\
-	niri						\
-	ghostty						\
-	gnome-keyring				\
-	dolphin						\
-	xwayland-satellite			\
-	ark							\
-	mako						\
-	mpv							\
-	unrar						\
-	gdm							\
-	xdg-desktop-portal-gtk		\
-	xdg-desktop-portal-gnome	\
-	cifs-utils					\
-	kio-fuse					\
-	kio-extras					\
-	dolphin-plugins				\
-	audiocd-kio					\
-	kf5-kimageformats			\
-	kdegraphics-thumbnailers	\
-	ffmpegthumbs				\
-	icoutils					\
-	taglib						\
-	cliphist					\
-	ddcutil						\
-	polkit-kde					\
-	gnome-keyring
+dnf5 -y install niri cava qt6-qtmultimedia cargo rust kde-connect
 
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install dms
+dnf5 -y copr disable avengemedia/dms
 
+dnf5 -y copr enable avengemedia/danklinux
+dnf5 -y install dsearch matugen dgop ghostty
+dnf5 -y copr disable avengemedia/danklinux
 
+#### enabling a System Unit File
 
-
-
-systemctl enable podman.socket
-systemctl --global add-wants niri.service mako.service
-systemctl --global add-wants niri.service plasma-polkit-agent.service
+systemctl --global add-wants niri.service dms dsearch.service
