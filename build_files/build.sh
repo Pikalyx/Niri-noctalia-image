2a5f5998717c8133513321099e79a3d10439027a
Pikalyx <pikalegend789@gmail.com>
2026-08-30 21:19:30 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 -y install niri cava qt6-qtmultimedia cargo rust kde-connect
+dnf5 -y install niri cava qt6-qtmultimedia cargo rust kde-connect gdm
 
 dnf5 -y copr enable avengemedia/dms
 dnf5 -y install dms
@@ -19,6 +19,23 @@ dnf5 -y copr enable avengemedia/danklinux
 dnf5 -y install dsearch matugen dgop ghostty
 dnf5 -y copr disable avengemedia/danklinux
 
-#### enabling a System Unit File
+# Ensure the graphical login manager is enabled so the user reaches a desktop
+systemctl enable gdm
 
+# Start the compositor/session pieces for the user session
 systemctl --global add-wants niri.service dms dsearch.service
+
+# Make sure the session is usable from the display manager
+mkdir -p /etc/dconf/profile
+cat > /etc/dconf/profile/user <<'EOF'
+user-db:user
+system-db:local
+EOF
+
+mkdir -p /etc/dconf/db/local.d
+cat > /etc/dconf/db/local.d/00-defaults <<'EOF'
+[org/gnome/desktop/interface]
+color-scheme='prefer-dark'
+EOF
+
+dconf update
